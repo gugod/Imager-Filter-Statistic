8bbfbc491bc73ab1677ed54f8e83e9cccd230e08
Kang-min Liu <gugod@gugod.org>
2014-12-02 00:32:35 +0100

    add geometry arg.

--- examples/convert-all.pl
+++ examples/convert-all.pl
@@ -4,14 +4,17 @@ use warnings;
 use Imager;
 use Imager::Filter::Statistic;
 
-my ($file, $output_dir) = @ARGV;
+my ($file, $output_dir, $geometry) = @ARGV;
+-f $file or die;
+$output_dir ||= "/tmp";
+$geometry ||= "3x3";
 
 my $img = Imager->new(file => $file) or die Imager->errstr;
 
 for my $method (qw< gradient variance min max mean >) {
     print "doing $method\n";
     my $img_copy = $img->copy;
-    $img_copy->filter( type => "statistic", method => $method, "geometry" => "3x3" );
+    $img_copy->filter( type => "statistic", method => $method, "geometry" => $geometry );
     $img_copy->write(file => "$output_dir/filter.$method.jpg");
     print "... done\n";
 }
